fafec14526be10b245c19dd0467ca90a3bab4385
Jefferson <jeffds126@gmail.com>
2025-04-14 21:40:34 -0300

    Adicionado telas de adicionar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'screens/home.dart';
+import 'screens/add_item.dart';
+import 'screens/edit_item.dart';
+import 'screens/add_topic.dart';
+import 'models/item.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => const Home()),
+      GoRoute(
+        path: '/add-item/:topicIndex',
+        builder: (context, state) {
+          final topicIndex = int.parse(state.pathParameters['topicIndex']!);
+          return AddItem(topicIndex: topicIndex);
+        },
+      ),
+      GoRoute(
+        path: '/edit-item/:topicIndex/:itemIndex',
+        builder: (context, state) {
+          final topicIndex = int.parse(state.pathParameters['topicIndex']!);
+          final itemIndex = int.parse(state.pathParameters['itemIndex']!);
+          final item = state.extra as Item;
+          return EditItem(
+            topicIndex: topicIndex,
+            itemIndex: itemIndex,
+            item: item,
+          );
+        },
+      ),
+      GoRoute(
+        path: '/add-topic',
+        builder: (context, state) => const AddTopic(),
+      ),
+      GoRoute(
+        path: '/edit-topic/:topicIndex',
+        builder: (context, state) {
+          final topicIndex = int.parse(state.pathParameters['topicIndex']!);
+          final topicName = state.extra as String;
+          return AddTopic(topicIndex: topicIndex, initialName: topicName);
+        },
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Lista de Compras',
       theme: ThemeData(
         primaryColor: Colors.teal[900],
@@ -38,12 +84,12 @@ class MyApp extends StatelessWidget {
         textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(foregroundColor: Colors.teal[400]),
         ),
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           bodyMedium: TextStyle(color: Colors.white70),
           titleLarge: TextStyle(color: Colors.white),
         ),
       ),
-      home: Home(),
+      routerConfig: _router,
     );
   }
 }
